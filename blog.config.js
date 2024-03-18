8b7ee971bed656e26912e6c42384af68370106e6
Lucas <Lookusluo@users.noreply.github.com>
2024-03-18 19:03:05 +0800

    update 2d model

--- blog.config.js
+++ blog.config.js
@@ -240,8 +240,8 @@ const BLOG = {
   WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || true, // 是否显示宠物挂件
   WIDGET_PET_LINK:
         process.env.NEXT_PUBLIC_WIDGET_PET_LINK ||
-        'https://github.com/Lookusluo/live2d-widget-models/blob/master/packages/live2d-widget-model-miku/assets/miku.model.json',
-		// 挂件模型地址 @see https://github.com/Lookusluo/live2d-widget-models
+        'https://cdn.jsdelivr.net/npm/live2d-widget-model-miku@1.0.5/assets/miku.model.json',
+		// 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
   WIDGET_PET_SWITCH_THEME: process.env.NEXT_PUBLIC_WIDGET_PET_SWITCH_THEME || false, // 点击宠物挂件切换博客主题
 
   // 音乐播放插件
